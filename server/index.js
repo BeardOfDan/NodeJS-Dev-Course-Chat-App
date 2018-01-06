786b98ef34760d4d121060860677edea3ca8b71a
Daniel Roberts <BeardOfDan@gmail.com>
2018-01-05 16:05:27 -0800

    (feat) Perform initial app set up

---
+++ server/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public/');
+
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
