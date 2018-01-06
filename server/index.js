1f82e2fef3cb18774a12445b234b5c0791ad99d8
Daniel Roberts <BeardOfDan@gmail.com>
2018-01-05 16:36:27 -0800

    (feat) Implement basic socket.io connection

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public/');
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected');
+  })
+});
 
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
