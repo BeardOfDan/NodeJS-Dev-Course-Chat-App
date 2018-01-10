fa6a7620c14e502cbc13cf19c2ae669014606816
Daniel Roberts <BeardOfDan@gmail.com>
2018-01-10 01:43:02 -0800

    (feat) Finish writing methods for Users class

--- server/utils/users.js
+++ server/utils/users.js
@@ -15,21 +15,62 @@ class Users {
   }
 
   addUser(id, name, room) {
+    // ensure that the user id is unique
+    if (this._getIndex(id) !== null) {
+      return null;
+    }
+
     const newUser = new User(id, name, room);
     this.list.push(newUser);
 
     return newUser;
   }
 
-  removeUser(id) {
+  // a helper method
+  _getIndex(id) {
+    // TODO: Ensure that the list array is sorted by the user id
+    //       This will allow me to do a binary search and have a time complexity of Log2(n)
+    //       instead of linear
 
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].id === id) {
+        return i;
+      }
+    }
+
+    return null;
+  }
+
+  removeUser(id) {
+    const index = this._getIndex(id);
+
+    if (index !== null) {
+      return this.list.splice(index, 1)[0];
+    } else {
+      return null;
+    }
   }
 
   getUser(id) {
+    const index = this._getIndex(id);
 
+    if (index !== null) {
+      return this.list[index];
+    } else {
+      return null;
+    }
   }
 
   getUserList(room) {
+    const users = this.list.filter((user, index, list) => {
+      return user.room === room;
+    });
+
+
+    return users.reduce((memo, user, list) => {
+      memo.push(user.name);
+      return memo;
+    }, []);
 
   }
 }
